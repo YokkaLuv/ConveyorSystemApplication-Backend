4b7ded45ed61aa5b004e8efb96c8aed05aaa07f1
daobathanhh <118471153+daobathanhh@users.noreply.github.com>
2025-04-04 00:05:14 +0700

    initialize SpringBoot and Mongodb

---
+++ src/main/java/com/server/mechacal/MechacalApplication.java
@@ -0,0 +1,13 @@
+package com.server.mechacal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechacalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechacalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/mechacal/MechacalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.mechacal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechacalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
